f9bbe64425954996495952d232d7b600f4829c2c
Atharva Kasture <pkmn.atharva@gmail.com>
2025-07-30 17:38:09 -0400

    Added routes of delete and update function to cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -5,12 +5,21 @@ const express = require('express');                      //Importing express fra
 const router = express.Router();                         //Creating a new router instance
 const { getAllCards } = require('../controllers/cardsController');    // Importing the controller function for handling card requests
 const { createCard } = require('../controllers/cardsController'); // Importing the controller function for creating a new card
+const { deleteCard } = require('../controllers/cardsController'); // Importing the controller function for deleting a card
+const { updateCard } = require('../controllers/cardsController'); // Importing the controller function for updating a card
 
 // Define a route to handle GET requests for all decision cards
 router.get('/', getAllCards);
 
 // Route to handle POST requests for creating a new decision card
 router.post('/', createCard); 
+
+// Route to delete a card by ID
+router.delete('/:id', deleteCard);
+
+// Route to update a card by ID
+router.put('/:id', updateCard);
+
 
 // Export the router so it can be used in other parts of the application such as the main app file
 module.exports = router;
